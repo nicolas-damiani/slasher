7698d4f2df302346c946ff769fa377d32f37244f
Gonzalo Kurin <gonzalokurin@gmail.com>
2018-09-12 18:27:44 -0300

    no message

--- Slasher/Slasher.Client/ClientLogic.cs
+++ Slasher/Slasher.Client/ClientLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Slasher.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,26 @@ namespace Slasher.Client
 {
     public class ClientLogic
     {
+        private List<User> userList;
+
+        public ClientLogic()
+        {
+            userList = new List<User>();
+        }
+
+        public bool createUser(string nickname, string avatar)
+        {
+            User user = new User(nickname, avatar);
+            if (!userList.Contains(user))
+            {
+                userList.Add(user);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
     }
 }
--- Slasher/Slasher.Entities/User.cs
+++ Slasher/Slasher.Entities/User.cs
@@ -8,8 +8,25 @@ namespace Slasher.Entities
 {
     public class User
     {
-        public string NickName { get; set; }
+        string NickName { get; set; }
         public string Avatar { get; set; }
         public Character Character { get; set; }
+
+        public User(string nickname, string avatar)
+        {
+            NickName = nickname;
+            Avatar = avatar;
+        }
+        
+        public override bool Equals(object objectToCompare)
+        {
+            if (objectToCompare == null || !objectToCompare.GetType().Equals(this.GetType()))
+                return false;
+            else
+            {
+                User compareUser = (User)objectToCompare;
+                return this.NickName.Equals(compareUser.NickName);
+            }
+        }
     }
 }
