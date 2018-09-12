406582e789db3f98911c5bd68b2950728193ed93
Nicolas Damiani <nico_damiani@hotmail.com>
2018-09-12 17:15:59 -0300

    no message

---
+++ Slasher/Slasher.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slasher.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Slasher/Slasher.Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slasher.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
