a91f21e7c8f3cd744e80ba544fbb1a3117318c78
Nicolas Damiani <nico_damiani@hotmail.com>
2018-09-26 18:58:09 -0300

    no message

--- Slasher/Slasher.Server/ServerLogic.cs
+++ Slasher/Slasher.Server/ServerLogic.cs
@@ -130,10 +130,16 @@ namespace Slasher.Server
             {
                 if (actionType == ActionType.MOVEMENT)
                 {
-                    Match.MovePlayer(user, Match.MovementCommands[direction]);
-                    byte[] responseStream = Protocol.GenerateStream(Protocol.SendType.RESPONSE, "41", "200");
-                    nws.Write(responseStream, 0, responseStream.Length);
-                }else if(actionType == ActionType.ATTACK)
+                    if (Match.MovementCommands.ContainsKey(direction))
+                    {
+                        Match.MovePlayer(user, Match.MovementCommands[direction]);
+                        byte[] responseStream = Protocol.GenerateStream(Protocol.SendType.RESPONSE, "41", "200");
+                        nws.Write(responseStream, 0, responseStream.Length);
+                    }
+                    else
+                        sendError(nws, "Comando invalido");
+                }
+                else if (actionType == ActionType.ATTACK)
                 {
                     //ATTACK
                 }
@@ -158,11 +164,11 @@ namespace Slasher.Server
             catch (SurvivorsWinException)
             {
                 string winnersString = "";
-                foreach(User winner in Match.Winners)
+                foreach (User winner in Match.Winners)
                 {
                     winnersString += winner.NickName + ", ";
                 }
-                byte[] responseStream = Protocol.GenerateStream(Protocol.SendType.RESPONSE, "61", "300|"+winnersString);
+                byte[] responseStream = Protocol.GenerateStream(Protocol.SendType.RESPONSE, "61", "300|" + winnersString);
                 nws.Write(responseStream, 0, responseStream.Length);
             }
             catch (MonsterWinsException)
